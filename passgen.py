80757ee5efb055d8f912e35805e62e0acbbebb73
carritox-lab <caritolopmol@gmail.com>
2025-11-20 17:18:05 +0000

    Passgen-entrophy and classes

--- passgen.py
+++ passgen.py
@@ -1,13 +1,21 @@
-def password_generator(phrase: str, number: str)-> str:
+import math
+from collections import Counter
+
+def password_generator(context:str, phrase: str, number: str)-> str:
     '''
     Generate a password based on the arguments given by the user
     '''
+    min_lengt, entropy_target = account_type(context)
     words = phrase.split()
     initials = "".join(word[0] for word in words) #obtain a string with the first letter of each word
     initials_with_sym = replace_letters_with_symbols(initials)
     capitalized_initials = capitilize_2rd_character(initials_with_sym)
 
-    return str(capitalized_initials) + number
+    password = str(capitalized_initials) + number
+    
+    while entropy_test(password) < entropy_target:
+        password = password #por poner algo
+    return password
 
 def replace_letters_with_symbols(initials: str)-> str:
     '''
@@ -32,8 +40,26 @@ def capitilize_2rd_character(text: str)-> str:
 
     return "".join(characters)
 
+def account_type(text: str)-> tuple:
+    '''
+    Associates the use of the password with their minimun lenght and entropy target
+    '''
+    types = {'banking': (20, 90), 'email': (16, 70), 'gaming': (12, 50), 'social media': (18, 80),}
+    
+    while text not in types:
+        text = input("Whats your password for?(banking, email, gaming, social media)")
+
+    return types.get(text)
+
+def entropy_test(text: str)-> int:
+    counts = Counter(text)
+    total = len(text)
+    entropy = -sum((count/total) * math.log2(count/total) for count in counts.values())
+    return entropy
+
 
 if __name__ == "__main__":
+    context = input("Whats your password for?(banking, email, gaming, social media)")
     phrase = input("Enter a sentence: ").strip().lower()
     number = input("Enter a number: ").strip()
-    print("Your password is: ", password_generator(phrase, number))
+    print("Your password is: ", password_generator(context, phrase, number))
